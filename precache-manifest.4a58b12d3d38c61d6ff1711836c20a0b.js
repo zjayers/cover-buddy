eea6ecb87bef48b3447ebab8830e515c06fb1bc7
Zach Ayers <zayers.dev@gmail.com>
2020-08-03 17:38:38 -0500

    Updates

---
+++ precache-manifest.4a58b12d3d38c61d6ff1711836c20a0b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf39973cc3560c1c30e3c39aa31609c",
+    "url": "/cover-buddy/index.html"
+  },
+  {
+    "revision": "0f31c965bca3de5c83a7",
+    "url": "/cover-buddy/static/css/2.add10c0e.chunk.css"
+  },
+  {
+    "revision": "d1e7bd16660a5509bb70",
+    "url": "/cover-buddy/static/css/main.44b11144.chunk.css"
+  },
+  {
+    "revision": "0f31c965bca3de5c83a7",
+    "url": "/cover-buddy/static/js/2.6a65039e.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/cover-buddy/static/js/2.6a65039e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e7bd16660a5509bb70",
+    "url": "/cover-buddy/static/js/main.408f0bc9.chunk.js"
+  },
+  {
+    "revision": "f99dfec7ade757d75328",
+    "url": "/cover-buddy/static/js/runtime-main.47cde3de.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/cover-buddy/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/cover-buddy/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/cover-buddy/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/cover-buddy/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/cover-buddy/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/cover-buddy/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/cover-buddy/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/cover-buddy/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/cover-buddy/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/cover-buddy/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/cover-buddy/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/cover-buddy/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/cover-buddy/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/cover-buddy/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/cover-buddy/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/cover-buddy/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
